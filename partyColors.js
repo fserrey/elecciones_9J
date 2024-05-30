efb7172b50b731ac48ef8c1acdca175b4aab94b8
Fran <francisco.serrano@galeo.tech>
2024-05-30 18:43:24 +0200

    first commit

---
+++ partyColors.js
@@ -0,0 +1,45 @@
+window._partyColors = {
+    "UP": "#9370DB",
+    "SUMAR": "#e51c55",
+    "PSOE": "#e02020", // #DA291C
+    "PP": "#02a2dd", // #17589D
+    "VOX": "#7CBD2A",
+    "CS": "#ea8046",
+    "JXCAT": "#3FE1D2",
+    "JXCATJUNTS": "#3FE1D2",
+    "JUNTSXCAT": "#3FE1D2",
+    "JUNTS": "#3FE1D2",
+    "ERC": "#FFB232",
+    "CUP": "#FEED14",
+    "PAR": "#fea000",
+    "NA+": "#1c699C",
+    "GBAI": "#F75E42",
+    "GEROABAI": "#F75E42",
+    "PNV": "#16823b", // #00902F
+    "BILDU": "#c7d892", // #08A3A6
+    "EH-BILDU": "#c7d892", // #08A3A6
+    "PRC": "#C2CE0A",
+    "FORO": "#17375e",
+    "BNG": "#76B2E1",
+    "CCA": "#FEED15", // #00ADE9
+    "CCAPNCNC": "#FEED15", // #00ADE9
+    "UPL": "#C71585",
+    "PACMA": "#00FF7F",
+    "TERUELEXISTE": "#007F51",
+    "PUMJ": "#F9CB9C",
+    "UPN": "#E51A22",
+    "PSC": "#e02020", 
+    "ECP": "#9370DB",
+    "ECP-PEC": "#9370DB",
+    "PDECAT":"#006EB1",
+    "ALIANZACAT":"#174F82",
+    
+    "PCTE": "#E3061A",
+    "EXISTE": "#007F51",
+    "ADELANTEANDALUCIA": "#24C87E",
+    "CAMINANDOJUNTOS": "#233B90",
+    "CPM": "#719430", // Coalición por melilla
+    "FO": "#B11E1A", // Frente obrero
+    "JM": "#286B3D", // Jaén merece más
+    "DO": "#FFCC00", // Democracia Ourensana
+}
